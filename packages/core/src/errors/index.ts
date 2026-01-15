e683c1da6f41792b8d3ac016e8153dd258f7d6c0
Toby Hede <toby@cipherstash.com>
2026-01-15 17:04:38 +1100

    feat(core): export error code system from main errors module

---
+++ packages/core/src/errors/index.ts
@@ -0,0 +1,3 @@
+export { ErrorCategory, ErrorCodes, type ErrorCodeDefinition, type ErrorCodeKey } from './codes.js';
+export { RundownError, type ErrorContext } from './rundown-error.js';
+export { Errors } from './factory.js';
